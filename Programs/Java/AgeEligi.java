a32b9e19adbd01bebfcff267b47580161f325b06
Achyut <48128719+MinorMist@users.noreply.github.com>
2023-05-28 00:43:35 +0530

    Create AgeEligi.java

---
+++ Programs/Java/AgeEligi.java
@@ -0,0 +1,10 @@
+public class AgeEligi {
+public static void main(String[] args) {
+int age=19;
+if(age>=18) {
+System.out.println("eligible for voting");
+}else {
+System.out.println("not eligible for voting");
+}
+}
+}
