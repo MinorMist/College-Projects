184e4de18f4cecbc0a892cd6e7829582c34e6e1d
Achyut <48128719+MinorMist@users.noreply.github.com>
2023-12-14 13:14:53 +0530

    medicine reminder app

---
+++ PillRem/app/src/main/java/com/project/pillrem/MedicineAdapter.java
@@ -0,0 +1,111 @@
+package com.project.pillrem;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
+
+class MedicineAdapter extends RecyclerView.Adapter<MedicineViewHolder>
+        implements Filterable {
+    private Context context;
+    private List<Medicine> listMeds;
+    private List<Medicine> mArrayList;
+    private DatabaseHandler mDatabase;
+    private String time;
+
+    MedicineAdapter(Context context, List<Medicine> listMeds) {
+        this.context = context;
+        this.listMeds = listMeds;
+        this.mArrayList = listMeds;
+        mDatabase = new DatabaseHandler(context);
+    }
+
+    @Override
+    public MedicineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.med_item, parent, false);
+        return new MedicineViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MedicineViewHolder holder, int position) {
+        final Medicine medicine = listMeds.get(position);
+        holder.medname.setText(medicine.getName());
+
+        if(Integer.parseInt(medicine.gettime())<46800000){
+            int timeh=(Integer.parseInt(medicine.gettime())/60000)/60;
+            int timem=(Integer.parseInt(medicine.gettime())/60000)-(timeh*60);
+            if (timeh==0) timeh=12;
+            if (timem<10)
+                time=timeh+":0"+timem+" AM";
+            else
+                time=timeh+":"+timem+" AM";
+        }
+        else{
+            int timeh=((Integer.parseInt(medicine.gettime())-43200000)/60000)/60;
+            int timem=((Integer.parseInt(medicine.gettime())-43200000)/60000)-(timeh*60);
+            if (timem<10)
+                time=timeh+":0"+timem+" PM";
+            else
+                time=timeh+":"+timem+" PM";
+        }
+
+
+        holder.medtime.setText(time);
+        holder.medday.setText(medicine.getday());
+
+        holder.deleteMed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDatabase.deleteContact(medicine);
+                ((Activity) context).finish();
+                context.startActivity(((Activity) context).getIntent());
+            }
+        });
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    listMeds = mArrayList;
+                } else {
+                    ArrayList<Medicine> filteredList = new ArrayList<>();
+                    for (Medicine med : mArrayList) {
+                        if (med.getName().toLowerCase().contains(charString)) {
+                            filteredList.add(med);
+                        }
+                    }
+                    listMeds = filteredList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = listMeds;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                listMeds = (ArrayList<Medicine>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    @Override
+    public int getItemCount() {
+        return listMeds.size();
+    }
+}
---
+++ PillRem/app/src/main/java/com/project/pillrem/MedicineViewHolder.java
@@ -0,0 +1,18 @@
+package com.project.pillrem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+class MedicineViewHolder extends RecyclerView.ViewHolder {
+    TextView medname, medtime,medday;
+    ImageView deleteMed;
+    ImageView editContact;
+    MedicineViewHolder(View itemView) {
+        super(itemView);
+        medname = itemView.findViewById(R.id.medname);
+        medtime = itemView.findViewById(R.id.medtime);
+        medday=itemView.findViewById(R.id.medday);
+        deleteMed = itemView.findViewById(R.id.deleteContact);
+    }
+}
